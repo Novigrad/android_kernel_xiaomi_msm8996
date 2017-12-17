3c40e2188aa737fde1ff147fc7f420346ee30168
MOVZX <movzx@yahoo.com>
2017-12-17 17:51:30 +0300

    Clean up kernel name/version string
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- init/version.c
+++ init/version.c
@@ -46,5 +46,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (" "MOVZX" "@" "Telegram.org" ")"
 	" (" LINUX_COMPILER ") %s\n";
