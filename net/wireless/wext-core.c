d14fbb336c23d23b346056004f8c3fd578ca2f05
MOVZX <movzx@yahoo.com>
2018-05-01 22:26:37 +0700

    Revert "Fix Various Uninitialised Warnings"
    This reverts commit d4d2041de1bc1832e97baecd03def1494a93692d.
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- net/wireless/wext-core.c
+++ net/wireless/wext-core.c
@@ -662,7 +662,7 @@ static int iw_handler_get_iwstats(struct net_device *		dev,
 				  char *			extra)
 {
 	/* Get stats from the driver */
-	struct iw_statistics *stats = NULL;
+	struct iw_statistics *stats;
 
 	stats = get_wireless_stats(dev);
 	if (stats) {
