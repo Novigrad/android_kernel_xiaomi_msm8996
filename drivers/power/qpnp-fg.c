9954b693b852ce7fa32641b0644029a66f3b6af0
MOVZX <movzx@yahoo.com>
2018-05-28 19:49:58 +0700

    Revert "power: qpnp-fg: queue on power efficient"
    This reverts commit 631035d4601282c66f45a3b00dd141cf906583f2.
    
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -2658,7 +2658,7 @@ static void check_sanity_work(struct work_struct *work)
 	rc = read_beat(chip, &beat_count);
 	if (rc == 0 && chip->last_beat_count != beat_count) {
 		chip->last_beat_count = beat_count;
-		queue_delayed_work(system_power_efficient_wq,
+		schedule_delayed_work(
 			&chip->check_sanity_work,
 			msecs_to_jiffies(SANITY_CHECK_PERIOD_MS));
 		return;
